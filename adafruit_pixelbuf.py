cece112f75bed17bf7b39643d4bfb4f82113c7ed
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:16 -0400

    Update version string

--- adafruit_pixelbuf.py
+++ adafruit_pixelbuf.py
@@ -20,7 +20,7 @@ try:
 except ImportError:
     pass
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_Pixelbuf.git"
 
 DOTSTAR_LED_START_FULL_BRIGHT = 0xFF
